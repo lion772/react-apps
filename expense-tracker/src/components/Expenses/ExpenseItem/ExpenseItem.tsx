5b4bb371bcb6aa1e30c4ba5226c30e9823e85926
william <william.steinkedemello@gmail.com>
2022-10-30 15:51:49 +0100

    add useState in ExpenseItem

--- expense-tracker/src/components/Expenses/ExpenseItem/ExpenseItem.tsx
+++ expense-tracker/src/components/Expenses/ExpenseItem/ExpenseItem.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import Card from "../../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import style from "./ExpenseItem.module.css";
@@ -13,14 +13,19 @@ const ExpenseItem: FC<IExpenseProps> = ({
     date,
     title,
     amount,
-}): JSX.Element => (
-    <Card className={style.ExpenseItem}>
-        <div className={style.ExpenseItem__description}>
-            <ExpenseDate date={date} />
-            <h2>{title}</h2>
-            <div className={style.ExpenseItem__price}>${amount}</div>
-        </div>
-    </Card>
-);
+}): JSX.Element => {
+    const [titleState, setTitle] = useState<string | null>(title);
+
+    return (
+        <Card className={style.ExpenseItem}>
+            <div className={style.ExpenseItem__description}>
+                <ExpenseDate date={date} />
+                <h2>{titleState}</h2>
+                <div className={style.ExpenseItem__price}>${amount}</div>
+            </div>
+            <button onClick={() => setTitle("New Title")}>Change Title</button>
+        </Card>
+    );
+};
 
 export default ExpenseItem;
