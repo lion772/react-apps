d52ed6029e789749ff45101356f1584edf9f9442
william <william.steinkedemello@gmail.com>
2022-10-31 08:45:08 +0100

    add form

--- expense-tracker/src/components/NewExpense/ExpenseForm.tsx
+++ expense-tracker/src/components/NewExpense/ExpenseForm.tsx
@@ -4,37 +4,58 @@ import styles from "./NewExpense.module.css";
 interface ExpenseFormProps {}
 
 const ExpenseForm: FC<ExpenseFormProps> = () => {
-    //const [title, setTitle] = useState<string>("");
-    //const [date, setDate] = useState<string>("");
-    //const [amount, setAmount] = useState<string>("");
+    const [title, setTitle] = useState<string>("");
+    const [date, setDate] = useState<string>("");
+    const [amount, setAmount] = useState<string>("");
 
-    const [userInput, setUserInput] = useState({
+    /*const [userInput, setUserInput] = useState({
         expenseName: "",
         date: "",
         amount: "",
-    });
+    });*/
 
-    const titleChangeHandler = (e: React.FormEvent<HTMLInputElement>) =>
-        setUserInput({ ...userInput, expenseName: e.currentTarget.value });
-    const dateChangeHandler = (e: React.FormEvent<HTMLInputElement>) =>
-        setUserInput({ ...userInput, date: e.currentTarget.value });
-    const amountChangeHandler = (e: React.FormEvent<HTMLInputElement>) =>
-        setUserInput({ ...userInput, amount: e.currentTarget.value });
+    const titleChangeHandler = (e: React.FormEvent<HTMLInputElement>) => {
+        //setUserInput({ ...userInput, expenseName: e.currentTarget.value });
+        setTitle(e.currentTarget.value);
+    };
+    const dateChangeHandler = (e: React.FormEvent<HTMLInputElement>) => {
+        //setUserInput({ ...userInput, expenseName: e.currentTarget.value });
+        setDate(e.currentTarget.value);
+    };
+    const amountChangeHandler = (e: React.FormEvent<HTMLInputElement>) => {
+        //setUserInput({ ...userInput, expenseName: e.currentTarget.value });
+        setAmount(e.currentTarget.value);
+    };
 
-    const clickHandler = (e: any) => {
+    const submitHandler = (e: React.SyntheticEvent) => {
         e.preventDefault();
+        /* 1° way of getting form data:
+        const target = e.target as typeof e.target & {
+            title: { value: string };
+            date: { value: string };
+            amount: { value: string };
+        };
+        const titleFormValue = target.title.value;
+        const dateFormValue = target.date.value;
+        const amountFormValue = target.amount.value;
+        console.log(titleFormValue, dateFormValue, amountFormValue);*/
+
+        //2° way of getting form data:
+        const expenseData = {
+            title: title,
+            date: new Date(date),
+            amount: amount,
+        };
+        console.log(expenseData);
     };
 
     return (
-        <form>
-            <div>
-                Test: {userInput.expenseName} {userInput.date}{" "}
-                {userInput.amount}
-            </div>
+        <form onSubmit={(e) => submitHandler(e)}>
             <div className={styles.NewExpense__controls}>
                 <div className={styles.NewExpense__control}>
                     <label>Title</label>
                     <input
+                        name="title"
                         type="text"
                         onChange={(e) => titleChangeHandler(e)}
                     />
@@ -42,6 +63,7 @@ const ExpenseForm: FC<ExpenseFormProps> = () => {
                 <div className={styles.NewExpense__control}>
                     <label>Amount</label>
                     <input
+                        name="amount"
                         type="number"
                         min="0.01"
                         step="0.01"
@@ -51,6 +73,7 @@ const ExpenseForm: FC<ExpenseFormProps> = () => {
                 <div className={styles.NewExpense__control}>
                     <label>Date</label>
                     <input
+                        name="date"
                         type="date"
                         min="2019-01-01"
                         max="2024-12-31"
@@ -58,11 +81,7 @@ const ExpenseForm: FC<ExpenseFormProps> = () => {
                     />
                 </div>
             </div>
-            <button
-                className={`styles.NewExpense button`}
-                type="submit"
-                onClick={(e) => clickHandler(e as any)}
-            >
+            <button className={`styles.NewExpense button`} type="submit">
                 Add new expense
             </button>
         </form>
--- expense-tracker/src/components/NewExpense/NewExpense.tsx
+++ expense-tracker/src/components/NewExpense/NewExpense.tsx
@@ -6,7 +6,6 @@ import styles from "./NewExpense.module.css";
 interface NewExpenseProps {}
 
 const NewExpense: FC<NewExpenseProps> = () => {
-    
     return (
         <div className={styles.NewExpense}>
             <ExpenseForm />
