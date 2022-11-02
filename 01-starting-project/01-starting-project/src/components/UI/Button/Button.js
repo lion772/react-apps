d0364d5ef3d650873c87e3af546116c0ce0c0582
william <william.steinkedemello@gmail.com>
2022-11-02 15:51:23 +0100

    dynamic styles with CSS modules

--- 01-starting-project/01-starting-project/src/components/UI/Button/Button.js
+++ 01-starting-project/01-starting-project/src/components/UI/Button/Button.js
@@ -1,7 +1,9 @@
 import React from "react";
-import styled from "styled-components";
+import styles from "./Button.module.css";
+// import styled from "styled-components";
 
-const Button = styled.button`
+/*const Button = styled.button`
+    width: 100%;
     font: inherit;
     padding: 0.5rem 1.5rem;
     border: 1px solid #8b005d;
@@ -14,20 +16,24 @@ const Button = styled.button`
         outline: none;
     }
 
-    .button:hover,
-    .button:active {
+    &:button:hover,
+    &:button:active {
         background: #ac0e77;
         border-color: #ac0e77;
         box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
     }
-`;
 
-/*const Button = props => {
-  return (
-    <button type={props.type} className="button" onClick={props.onClick}>
-      {props.children}
-    </button>
-  );
-};*/
+    @media (min-width: 768px) {
+        width: auto;
+    }
+`;*/
+
+const Button = (props) => {
+    return (
+        <button type={props.type} className={styles.button} onClick={props.onClick}>
+            {props.children}
+        </button>
+    );
+};
 
 export default Button;
