f543a6a3bb131cbd411300fcd10d264b4207ba95
william <william.steinkedemello@gmail.com>
2022-11-05 14:44:31 +0100

    add context

--- 03-handling-side-effects/src/components/MainHeader/Navigation.js
+++ 03-handling-side-effects/src/components/MainHeader/Navigation.js
@@ -1,29 +1,39 @@
-import React from 'react';
+import React from "react";
+import AuthContext from "../../store/auth-context";
 
-import classes from './Navigation.module.css';
+import classes from "./Navigation.module.css";
 
 const Navigation = (props) => {
-  return (
-    <nav className={classes.nav}>
-      <ul>
-        {props.isLoggedIn && (
-          <li>
-            <a href="/">Users</a>
-          </li>
-        )}
-        {props.isLoggedIn && (
-          <li>
-            <a href="/">Admin</a>
-          </li>
-        )}
-        {props.isLoggedIn && (
-          <li>
-            <button onClick={props.onLogout}>Logout</button>
-          </li>
-        )}
-      </ul>
-    </nav>
-  );
+    return (
+        <AuthContext.Consumer>
+            {({isLoggedIn}) => {
+                <>
+                    {console.log("isloggedIn", isLoggedIn)}
+                    <nav className={classes.nav}>
+                        <ul>
+                            {isLoggedIn && (
+                                <li>
+                                    <a href="/">Users</a>
+                                </li>
+                            )}
+                            {isLoggedIn && (
+                                <li>
+                                    <a href="/">Admin</a>
+                                </li>
+                            )}
+                            {isLoggedIn && (
+                                <li>
+                                    <button onClick={props.onLogout}>
+                                        Logout
+                                    </button>
+                                </li>
+                            )}
+                        </ul>
+                    </nav>
+                </>;
+            }}
+        </AuthContext.Consumer>
+    );
 };
 
 export default Navigation;
