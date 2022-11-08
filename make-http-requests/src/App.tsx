18580583cbc0ef2510fb3c68f6e21034b2c250f9
william <william.steinkedemello@gmail.com>
2022-11-08 13:53:53 +0100

    add error handling

--- make-http-requests/src/App.tsx
+++ make-http-requests/src/App.tsx
@@ -9,41 +9,55 @@ type MovieAPI = {
     release_date: string;
 };
 
-const API_FILMS = "https://swapi.dev/api/films";
+const API_FILMS = "https://swapi.dev/api/filmss";
 
 const App: FC = () => {
     const [movies, setMovies] = useState([]);
     const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [error, setError] = useState<string | null>();
 
     async function fetchMoviesHandler() {
         setIsLoading(true);
-        const response = await fetch(API_FILMS);
-        const { results } = await response.json();
-        const transformedMovies = results.map((movie: MovieAPI) => {
-            return {
-                id: movie.episode_id,
-                title: movie.title,
-                openingText: movie.opening_crawl,
-                releaseDate: movie.release_date,
-            };
-        });
-        console.log(transformedMovies);
-        setMovies(transformedMovies);
+        setError(null);
+
+        try {
+            const response = await fetch(API_FILMS);
+            if (response.status !== 200) {
+                throw new Error("Something went wrong!");
+            }
+            const { results } = await response.json();
+            const transformedMovies = results.map((movie: MovieAPI) => {
+                return {
+                    id: movie.episode_id,
+                    title: movie.title,
+                    openingText: movie.opening_crawl,
+                    releaseDate: movie.release_date,
+                };
+            });
+
+            console.log(transformedMovies);
+            setMovies(transformedMovies);
+        } catch (e) {
+            setError((e as Error).message);
+        }
+
         setIsLoading(false);
     }
+
+    let content = <p>Found no movies.</p>;
+
+    if (movies.length > 0) content = <MoviesList movies={movies} />;
+
+    if (error) content = <p>{error}</p>;
+
+    if (isLoading) content = <p>Loading...</p>;
 
     return (
         <>
             <section>
                 <button onClick={fetchMoviesHandler}>Fetch movies!</button>
             </section>
-            <section>
-                {!isLoading && movies.length > 0 && (
-                    <MoviesList movies={movies} />
-                )}
-                {!isLoading && movies.length === 0 && <p>Found no movies.</p>}
-            </section>
-            <section>{isLoading && <p>Loading...</p>}</section>
+            <section>{content}</section>
         </>
     );
 };
