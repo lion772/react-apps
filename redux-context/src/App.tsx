5a9fde0c5070c1716659a35eda947b2c43fd2a6f
william <william.steinkedemello@gmail.com>
2022-11-28 21:08:58 +0100

    Navigation created and set

--- redux-context/src/App.tsx
+++ redux-context/src/App.tsx
@@ -1,17 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route } from "react-router-dom";
+import "./App.css";
+import Navigation from "./components/Navigation/Navigation.lazy";
 
 function App() {
-  return (
-      <>
-          <Navigation />
-          <main>
-              <Route path="/" component={ProductsPage} exact />
-              <Route path="/favorites" component={FavoritesPage} />
-          </main>
-      </>
-  );
+    return (
+        <>
+            <Navigation />
+            <main>
+                <Route path="/" component={ProductsPage} exact />
+                <Route path="/favorites" component={FavoritesPage} />
+            </main>
+        </>
+    );
 }
 
 export default App;
