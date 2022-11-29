c65d31f2a550e13ecd10ae0f3893f5c0f90a6ba7
william <william.steinkedemello@gmail.com>
2022-11-29 15:53:04 +0100

    Custom hook as a store being implemented part 2

--- redux-context/src/hook-store/store.tsx
+++ redux-context/src/hook-store/store.tsx
@@ -2,10 +2,19 @@ import { useState, useEffect } from "react";
 
 let globalState = {};
 let listeners: any = [];
-let actions = {};
+let actions: any = {};
 
 export const useStore = () => {
     const setState = useState(globalState)[1];
+
+    const dispatch = (actionIdentifier: any) => {
+        const newState = actions[actionIdentifier](globalState);
+        globalState = { ...globalState, newState };
+
+        for (const listener of listeners) {
+            listener(globalState);
+        }
+    };
 
     useEffect(() => {
         listeners.push(setState);
@@ -14,4 +23,13 @@ export const useStore = () => {
             listeners = listeners.filter((li: any) => li !== setState);
         };
     }, [setState]);
+
+    return [globalState, dispatch];
+};
+
+export const initStore = (userActions: any, initialState: any) => {
+    if (!initialState) {
+        globalState = { ...globalState, ...initialState };
+    }
+    actions = { ...actions, ...userActions };
 };
